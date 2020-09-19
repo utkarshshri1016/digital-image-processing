0de84ed4fc93ac3fc464422375b862a6a94a9cd7
Utkarsh Shrivastava <56174665+utkarshshri1016@users.noreply.github.com>
2020-09-19 15:13:26 +0530

    Delete Translation.py

--- Translation.py
+++
@@ -1,9 +0,0 @@
-from cv2 import cv2
-import numpy as np
-img=cv2.imread("Resources/Data/lena.png")
-(height,width)=img.shape[:2]
-cv2.imshow("Original",img)
-translation_matrix=np.float32([[1,0,70],[0,1,110]])
-translation=cv2.warpAffine(img,translation_matrix,(height,width))
-cv2.imshow("Translated",translation)
-cv2.waitKey()
